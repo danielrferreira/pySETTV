8250502c755289e5c6562c613c18a70098ac1375
Daniel Ferreira <30530085+danielrferreira@users.noreply.github.com>
2023-09-21 13:00:35 -0400

    Create df_module_1.py

---
+++ 06 - Utility & References/Reference/Modules and Packages/df_pack/df_module_1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  1 16:47:49 2023
+
+@author: danielferreira
+"""
+
+def soma(x,y):
+    return x+y
+
+def multiplication(x,y):
+    return x*y
+
+z=1
